b1efe6b0d6ae68dbf881db7835384efaf55ae60a
Anil George <anil.george27@gmail.com>
2021-01-04 08:07:13 +0530

    ArithmeticOperators

---
+++ Data Types and Operators/ArithmeticOperators.py
@@ -0,0 +1,8 @@
+# Write an expression that calculates the average of 23, 32 and 64
+# Place the expression in this print statement
+print((23+32+64)/3)
+# Two parts of a floor need tiling. One part is 9 tiles wide by 7 tiles long, the other is 5 tiles wide by 7 tiles long. Tiles come in packages of 6.
+# 1. How many tiles are needed?
+print(9*7+5*7)
+# 2. You buy 17 packages of tiles containing 6 tiles each. How many tiles will be left over?
+print(17*6-(9*7+5*7))
