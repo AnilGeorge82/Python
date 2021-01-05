4808d160165382f08fb1c881ce0edbba8b98175d
Anil George <anil.george27@gmail.com>
2021-01-05 08:31:37 +0530

    VariablesAndAssignmentOperators

---
+++ Data Types and Operators/VariablesAndAssignmentOperators.py
@@ -0,0 +1,17 @@
+x = 2
+y = x
+print (y)
+mv_population = 74728
+print (mv_population)
+# calculate the population density when the area is 11.995
+mv_population = 74728
+mv_area = 11.995
+mv_density = mv_population / mv_area
+print (mv_density)
+#The pythonic way to name variables is to use all lowercase letters and underscores to separate words.
+mv_population = 74728
+mv_population += 4000 - 600
+print(mv_population)
+mv_population = 74728
+mv_population -= 4000
+print(mv_population)
