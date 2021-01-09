113aaa88da201bcddd466ebd955e1f74a53b24fc
Anil George <anil.george27@gmail.com>
2021-01-09 17:48:24 +0530

    Strings

---
+++ Data Types and Operators/Strings.py
@@ -0,0 +1,13 @@
+print("Hello World")
+print('Hello World')
+first_word = "Hello"
+second_word = "There"
+print(first_word + " " + second_word)
+word = "Hello"
+print(word * 10)
+count_length = len("count")
+print(count_length)
+string_with_quotes = "Mr. Y\'s house is nearby"
+print(string_with_quotes)
+ford_quote = 'Whether you think you can, or you think you can\'t--you\'re right.'
+print(ford_quote)
