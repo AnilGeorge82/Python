9aa1bbc24e2b2c7ac9eb41197de6cb58a4b3b126
Anil George <anil.george27@gmail.com>
2021-01-12 08:15:57 +0530

    StringMethods

---
+++ Data Types and Operators/StringMethods.py
@@ -0,0 +1,16 @@
+print("anil george".title())
+full_name = "anil george"
+print(full_name.isupper())
+print(full_name.islower())
+print("one fish, two fish, red fish, blue fish.".count('fish'))
+print("arnold schwarzenegger".islower())
+print("arnold schwarzenegger".count('a'))
+print("arnold schwarzenegger".find('a'))
+print("anil has {} balloons".format(27))
+# No professional has all the methods memorized, which is why understanding how to use documentation and find answers is so important. Gaining a strong grasp of the foundations of programming will allow you to use those foundations to use documentation to build so much more than someone who tries to memorize all the built-in methods in Python.
+animal = "dog"
+action = "jump"
+object = "fence"
+print("Does your {} {} over the {}?".format(animal, action, object))
+maria_string = "Maria loves {} and {}"
+print(maria_string.format("math", "statistics"))
