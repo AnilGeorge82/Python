de28e219409d9a83008054242afc87d49023fb89
Anil George <anil.george27@gmail.com>
2021-01-15 08:02:16 +0530

    ListsAndMembershipOperators

---
+++ Data Structures/ListsAndMembershipOperators.py
@@ -0,0 +1,10 @@
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+print(months[2])
+print(months[3])
+print(months[8])
+print(months[-5])
+
+print(months[5])
+
+list_of_random_things = [1, 3.5, 'a string', True]
+print(list_of_random_things[1])
