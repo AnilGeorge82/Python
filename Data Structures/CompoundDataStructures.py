aac07e1c5f36b52b270e43809dbf5ff6438b031e
Anil George <anil.george27@gmail.com>
2021-02-02 08:06:09 +0530

    ConditionalStatements

--- Data Structures/CompoundDataStructures.py
+++ Data Structures/CompoundDataStructures.py
@@ -1,12 +1,18 @@
 elements = {"hydrogen": {"number": 1,
                          "weight": 1.00794,
                          "symbol": "H"},
-              "helium": {"number": 2,
-                         "weight": 4.002602,
-                         "symbol": "He"}}
+            "helium": {"number": 2,
+                       "weight": 4.002602,
+                       "symbol": "He"}}
 helium = elements["helium"]
 hydrogen_weight = elements["hydrogen"]["weight"]
 print(hydrogen_weight)
-oxygen = {"number": 8, "weight": 15.999, "symbol":"O"}
+oxygen = {"number": 8, "weight": 15.999, "symbol": "O"}
 elements["oxygen"] = oxygen
 print('elements =', elements)
+
+# Data Structure	Ordered 	Mutable 	Constructor 	Example
+# List	            Yes	        Yes	        [ ] or list()	[5.7, 4, 'yes', 5.7]
+# Tuple	            Yes	        No	        ( ) or tuple()	(5.7, 4, 'yes', 5.7)
+# Set	            No	        Yes	        {}* or set()	{5.7, 4, 'yes'}
+# Dictionary	    No	        No**	    { } or dict()	{'Jun': 75, 'Jul': 89}
