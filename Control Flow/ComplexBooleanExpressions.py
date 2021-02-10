93287599a60fd21abce5d8cb57c7db025d6a3d4f
Anil George <anil.george27@gmail.com>
2021-02-10 19:51:21 +0530

    ComplexBooleanExpressions

---
+++ Control Flow/ComplexBooleanExpressions.py
@@ -0,0 +1,10 @@
+weight = 72
+height = 172
+if 18.5 <= weight / height**2 < 25:
+    print("BMI is considered 'normal'")
+
+age = 18
+old_enough_to_get_driving_licence = age >= 17
+print(old_enough_to_get_driving_licence)
+
+
