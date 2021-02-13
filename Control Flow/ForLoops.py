8a7c1737d207d0e17fb2e66bbb357174e551ee3d
Anil George <anil.george27@gmail.com>
2021-02-13 20:07:20 +0530

    ForLoops

---
+++ Control Flow/ForLoops.py
@@ -0,0 +1,23 @@
+cities = ['new york city', 'mountain view', 'chicago', 'los angeles']
+for city in cities:
+    print(city.title())
+
+cities = ['new york city', 'mountain view', 'chicago', 'los angeles']
+for index in range (len(cities)):
+    cities[index] = cities[index].title()
+print(cities)
+
+
+print(list(range(4)))
+
+sentence = ["the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+
+for word in sentence:
+    print(word)
+
+for i in range(5, 35, 5):
+    print(i)
+
+
+
+
