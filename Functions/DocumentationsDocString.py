6f8d1b609df0eebc68f13a4f178ee7056e73984b
Anil George <anil.george27@gmail.com>
2021-03-13 07:20:32 +0530

    DocString

---
+++ Functions/DocumentationsDocString.py
@@ -0,0 +1,48 @@
+def population_density(population, land_area):
+    """Calculate the population density of an area"""
+    return population / land_area
+    
+print(population_density)
+
+def population_density(population, land_area):
+    """Calculate the population density of an area.
+
+    INPUT:
+    population: int. The population of that area
+    land_area: int or float. This function is unit-agnostic, if you pass in values in terms
+    of square km or square miles the function will return a density in those units.
+
+    OUTPUT: 
+    population_density: population / land_area. The population density of a particular area.
+    """
+    return population / land_area
+
+def readable_timedelta(days):
+    """Return a string of the number of weeks and days included in days."""
+    weeks = days // 7
+    remainder = days % 7
+    return "{} week(s) and {} day(s)".format(weeks, remainder)
+
+def readable_timedelta(days):
+    """Return a string of the number of weeks and days included in days.
+
+    Args:
+        days (int): number of days to convert
+    """
+    weeks = days // 7
+    remainder = days % 7
+    return "{} week(s) and {} day(s)".format(weeks, remainder)
+
+def readable_timedelta(days):
+    """
+    Return a string of the number of weeks and days included in days.
+
+    Parameters:
+    days -- number of days to convert (int)
+
+    Returns:
+    string of the number of weeks and days included in days
+    """
+    weeks = days // 7
+    remainder = days % 7
+    return "{} week(s) and {} day(s)".format(weeks, remainder)
