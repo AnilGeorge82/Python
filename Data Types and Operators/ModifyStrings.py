a5d81b994953a28e8b416162ccb19a21090eebd7
Anil George <anil.george27@gmail.com>
2021-03-25 15:46:07 +0530

    ModifyStrings

---
+++ Data Types and Operators/ModifyStrings.py
@@ -0,0 +1,21 @@
+a = "Hello World!"
+print(a.upper())
+
+a = "Hello World!"
+print(a.lower())
+
+#Remove Whitespace
+a = " Hello World! "
+print(a.strip())
+
+#Replace String
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#Split String
+a = "Hello, World!"
+print(a.split(","))
+
+
+
+
