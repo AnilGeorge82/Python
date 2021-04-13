44287e0d24d00b94819baddc647a0a48b5216a15
Anil George <anil.george27@gmail.com>
2021-04-13 16:09:23 +0530

    CreatingVariables

---
+++ Getting Started with Python/CreatingVariables.py
@@ -0,0 +1,20 @@
+salary = 24000
+print(salary)
+house_rent = 10000
+food_exp = 5000
+total_exp = house_rent + food_exp
+print(total_exp)
+savings = salary - total_exp
+print(savings)
+print("My savings in a year is:", savings)
+
+course_fee = 299
+num_of_months = 6
+total_fee = course_fee * num_of_months  
+print(total_fee)
+
+num_1 = 2
+num_2 = 2
+total = num_1 + num_2
+print(total)
+
