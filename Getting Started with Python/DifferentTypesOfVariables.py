68f1229e0a95aee2b713a6251f1332551fe56ba3
Anil George <anil.george27@gmail.com>
2021-04-14 15:38:54 +0530

    DifferentTypesOfVariables

---
+++ Getting Started with Python/DifferentTypesOfVariables.py
@@ -0,0 +1,22 @@
+x = True
+y = False
+
+print('x and y is', x and y)
+print('x and y is', x or y)
+print('not x is:', not x)
+print('not y is:', not y)
+
+x = "awesome"
+print(x)
+print("Python is " + x)
+
+x = "Python is "
+y = "awesome "
+z = x + y
+print(z)
+
+a = z * 8
+print(a)
+
+print(len(x))
+print(len(a))
