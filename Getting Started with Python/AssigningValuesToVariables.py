cc9a9f527a1b6c85c045210a1475cd95ccb08d5c
Anil George <anil.george27@gmail.com>
2021-04-15 16:47:40 +0530

    AssigningValuesToVariables

---
+++ Getting Started with Python/AssigningValuesToVariables.py
@@ -0,0 +1,26 @@
+x = y = z = "Python"
+print(x)
+print(y)
+print(z)
+
+b = x + y
+print(b)
+
+x = y == z
+print(x)
+
+a, b, c = 5, 3.2, "Hello"
+print(a)
+print(b)
+print(c)
+
+color_a = 'Red'
+color_b = 'Blue'
+print(color_a , color_b)
+
+color_c = color_a
+print(color_a, color_b, color_c)
+
+color_a = color_b   
+print(color_a, color_b, color_c)
+
