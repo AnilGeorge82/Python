0a2daf60b2078cd0742b65e182bac073490b098d
Anil George <anil.george27@gmail.com>
2021-04-16 15:51:04 +0530

    UpdatingVariableValues

---
+++ Getting Started with Python/UpdatingVariableValues.py
@@ -0,0 +1,59 @@
+n1 = 34
+n2 = 34.5
+n3 = 100
+s1 = 'Hello'
+s2 = 'World'
+total_n = n1 + n2 + n3
+print(total_n)
+
+n1 = n1 + 1
+print(n1)
+
+n2 = n2 + 10
+print(n2)
+
+n3 = n3 - 50
+print(n3)
+
+n3 = n3 * 50
+print(n3)
+
+n3 = n3 / 50
+print(n3)
+
+n1 += n1 + 1
+print(n1)
+
+n2 += 10
+print(n2)
+
+n3 *= 50
+print(n3)
+
+n3 /= 50
+print(n3)
+
+concat_string = s1 + s2
+print(concat_string)
+
+s1 = s1 + 'yoohoo'
+print(s1)
+
+s2 = s2 + 'yippee'
+print(s2)
+
+new_s1 = s1
+print(new_s1)
+
+s1 = 'Some other value'
+print(s1)
+
+print(new_s1)
+
+some_n = n3
+print(some_n)
+
+n3 *= 1000
+print(n3)
+
+print(some_n)
