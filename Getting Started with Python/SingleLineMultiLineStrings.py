b068e5dacab27b18e2f3c193002d3091e28d509a
Anil George <anil.george27@gmail.com>
2021-04-22 17:53:38 +0530

    SingleLineMultiLineStrings

---
+++ Getting Started with Python/SingleLineMultiLineStrings.py
@@ -0,0 +1,20 @@
+print('I am a single quoted string')
+print("I am a double quoted string")
+print("python " + 'is ' + """awesome""")
+print("python " * 3)
+
+print("I'm learning python")
+
+print('I\'m learning python')
+print("John said \"Hello there!\"")
+print('I\'m learning "python"')
+print("Hello John! \nHow are you?")
+print("This is a very long line \nbut we can have it span \nmultiple lines.")
+print("""I am a triple quoted string""")
+
+#alternate solution to \n is '''   ''' or """   """
+print('''This is a very long
+string in python''') 
+print("""This is a very long
+string in python
+really very long""")
