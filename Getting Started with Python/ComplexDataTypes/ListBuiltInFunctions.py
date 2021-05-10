09fb462ca5371b0bd0586560859cf000ff238e7f
Anil George <anil.george27@gmail.com>
2021-05-10 17:37:16 +0530

    ListBuiltInFunctions

---
+++ Getting Started with Python/ComplexDataTypes/ListBuiltInFunctions.py
@@ -0,0 +1,29 @@
+list_num = [20, 70, 80, 50, 30, 60, 40]
+print(list_num)
+
+print(max(list_num))
+print(min(list_num))
+print(len(list_num))
+list_num.sort()
+
+list_num.append(25)
+print(list_num)
+
+sorted_list_num = sorted(list_num)
+print(sorted_list_num)
+sum(list_num)
+print(sum(list_num))
+
+list_num *= 2
+print(list_num)
+
+all([0])
+print(all([1]))
+print(all([0,1]))
+
+print(any([0]))
+print(any([1]))
+print(any([0, 1]))
+print(any(list_num))
+
+
