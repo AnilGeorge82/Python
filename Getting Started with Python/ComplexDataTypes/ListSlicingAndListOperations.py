8903ebe691b2d287b4d76073dba4f9cca2f543fa
Anil George <anil.george27@gmail.com>
2021-05-11 17:32:42 +0530

    ListSlicingAndListOperations

---
+++ Getting Started with Python/ComplexDataTypes/ListSlicingAndListOperations.py
@@ -0,0 +1,19 @@
+my_cars_list = ['Tuv 300' , 'Xuv 500', 'Xuv 300', 'Xuv 700', 'Bolero', 'Scorpio']
+print(my_cars_list)
+print(len(my_cars_list))
+print(my_cars_list[4])
+print(my_cars_list[5])
+print(my_cars_list[-2])
+print(my_cars_list[-4])
+
+#slicing - List Slicing a deep copy
+print(my_cars_list[0:4])
+print(my_cars_list[0:2])
+print(my_cars_list[0:8])
+print(my_cars_list[7:])
+print(my_cars_list[5:6])
+print(my_cars_list[3:6])
+print(my_cars_list[:3])
+print(my_cars_list[3:])
+print(my_cars_list[0:-4])
+print(my_cars_list[-4:-1])
