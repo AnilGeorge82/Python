1bf5b18b9403150854f105bf2f24bc24808154d7
Anil George <anil.george27@gmail.com>
2021-06-10 08:55:03 +0530

    WorkingWithNestedLists

---
+++ Getting Started with Python/ComplexDataTypes/WorkingWithNestedLists.py
@@ -0,0 +1,14 @@
+car_matrix = [['Hennessey Venom GT', 1244],
+              ['SSC Ultimate Aero', 1287],
+              ['Zenvo STI', 1100]]
+print(car_matrix)
+
+print(len(car_matrix))
+print(len(car_matrix[1]))
+print(len(car_matrix[2]))
+print(car_matrix[1][0])
+print(car_matrix[1][1])
+print(car_matrix[2][0])
+
+print(car_matrix[2][0:2])
+print(car_matrix[1][0:2])
