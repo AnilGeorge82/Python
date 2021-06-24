8bfb130e401a3b376eed92c38fc4583f439f8d47
Anil George <anil.george27@gmail.com>
2021-06-24 09:03:14 +0530

    DeepCopiesOfDictionaries

---
+++ Getting Started with Python/ComplexDataTypes/CreatingDeepCopiesOfDictionaries.py
@@ -0,0 +1,65 @@
+months = {'jan': 1, 'feb': 2, 'march': 3, 'april': 4}
+print(months)
+
+new_months = months.copy()
+print("Original: ", months)
+print("Modified: ", new_months)
+
+print(new_months['feb'])
+
+new_months['feb'] = 'FEB'
+print("Original: ", months)
+print("Modified: ", new_months)
+
+import copy
+
+employees = {"Alison" : 2007, "Bart" : 2008, "Cathy" : 2009, "Dan" : 2010}
+print(employees)
+
+employees_copy = copy.deepcopy(employees)
+print(employees)
+print(employees_copy)
+
+print(employees["Alison"])
+
+employees_copy["Alison"] = 2011
+print(employees)
+print(employees_copy)
+
+orig_dict = {'one' :1, 'two':2, 'three':3, 'four':[1,2,3,4.0]}
+print(orig_dict)
+
+new_dict = copy.deepcopy(orig_dict)
+print("Original: ", orig_dict)
+print("Modified: ",new_dict)
+
+new_dict['four'][3] = 'FOUR'
+print("Original: ", orig_dict)
+print("Modified: ",new_dict)
+
+#copy.copy(dict) produces a deep copy of the outer dictionary but a shallow copy of nested types
+
+new_dict['four'].append(5)
+print("Original: ", orig_dict)
+print("Modified: ",new_dict)
+
+orig_dict = {'one':1, 'two':2, 'three': {'zero':0, 'one': 1}, 'four':4}
+print(orig_dict)
+
+new_dict = copy.deepcopy(orig_dict)
+print(orig_dict)
+print(new_dict)
+
+new_dict['three']['zero'] = 'ZERO'
+print(orig_dict)
+print(new_dict)
+
+orig_dict = {'one':1, 'two':2, 'three': {0,1,2}, 'four':4}
+
+new_dict = copy.deepcopy(orig_dict)
+print('Original: ',orig_dict)
+print('Modified: ',new_dict)
+
+new_dict['three'].add('THREE')
+print('Original: ',orig_dict)
+print('Modified: ',new_dict)
