fbe0a3178981d309aa5a393f4affed3f0a435831
Anil George <anil.george27@gmail.com>
2021-06-26 09:10:51 +0530

    Conditions

---
+++ Getting Started with Python/ConditionalStatementAndLoops/Conditions.py
@@ -0,0 +1,75 @@
+print(10 == 10)  
+# = is for assignment == is for equality
+
+print(10 == 20) 
+print(10 == "Hello") 
+print("Hello" == "Hello") 
+
+# a == b checks whether the values stored in variable a and b are equal
+# a = b assignment operation, will assign the value stored in b to a
+
+print(20 != 20)
+print(20 != 10)
+
+x = "Hello"
+x == "Hello"
+print(x)
+
+print(x == "Python")
+print(x != "Python")
+
+print(10<20)
+
+print(10<9)
+
+print(10<10)
+
+print(15>20)
+
+print(20<=15)
+
+print(20<=20)
+
+print(20<20)
+
+print(10>=20)
+
+print(2 + 3 < 6)
+
+# Operator Precedence PEDMAS, BODMAS
+
+print((2 +3) < 6)
+
+print(8 + 5 > 10 + 2)
+
+print(8 + 5 == 10 + 3)
+
+print(2 * 5 > 10)
+
+print(2 * 5 >= 10)
+
+print(40 % 3 == 0)
+
+print(40 % 2 == 0)
+
+print(0 == 40 % 2)
+
+print( 5 > 3 and 4 < 6)
+
+print( 5 > 3) and (4 < 6)
+
+print( 5 > 3 and 4 < 2)
+
+print( 10 == 10 == True)
+
+print( 10 == 10 == 10 == True)
+
+print((10 == 10) == True)
+
+print( 5 > 3 or 4 < 2)
+
+print( 5 > 3 or 4 < 6)
+
+print(not( 5 > 3 or 4 < 6))
+
+
