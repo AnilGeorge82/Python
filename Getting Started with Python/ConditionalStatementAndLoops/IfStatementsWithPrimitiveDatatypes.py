cceab08dfc133410ac16edaf342ddcb58d7c0b83
Anil George <anil.george27@gmail.com>
2021-06-28 09:04:12 +0530

    IfStatementsWithPrimitiveDataTypes

---
+++ Getting Started with Python/ConditionalStatementAndLoops/IfStatementsWithPrimitiveDatatypes.py
@@ -0,0 +1,37 @@
+# If-Else statements help us make decisions and take actions appropriate with those decisions
+if 10 < 15:
+    print("10 is less than 15")
+
+if 46 > 40 + 5:
+    print("46 is greated than 45")
+
+if 46 > 40:
+    print("46 is greated than 40")
+
+x_value = 65
+y_value = 25
+
+if x_value < y_value:
+    print('Value of x is less than the value of y')
+
+if x_value > y_value:
+    print('Value of x is greater than the value of y')
+
+my_string = "Hello python world"
+if "python" in my_string:
+    print('Yes, "python" is in my_string')
+
+if "pythonworld" in my_string:
+    print('Yes, "python" is in my_string')
+
+if "python world" in my_string:
+    print('Yes, "python world" is in my_string')
+
+list_student = ["Ayden", "Gavin", "Ian", "Jose", "Jane"]
+print(list_student)
+
+if "Jose" in list_student:
+    print("Jose is a student")
+
+if "Eva" in list_student:
+    print("Jose is a student")
