7a0e3ef40367ab1dac04cc443afe0afbf69848d0
Anil George <anil.george27@gmail.com>
2021-07-06 09:11:04 +0530

    TypeConversionsWithPrimitiveDatatypes_I

---
+++ Getting Started with Python/ConditionalStatementAndLoops/TypeConversionswithPrimitiveDatatypes_I.py
@@ -0,0 +1,37 @@
+print(type(66))
+print(float(66))
+a = 66
+print(type(a))
+
+my_float = float(a)
+print(my_float)
+print(type(390.8))
+
+print(int(390.8))
+
+b = 66.7
+my_int = int(b)
+print(my_int)
+
+num_int = 76
+num_float = 23.45
+
+print("datatype of num_int:", type(num_int))
+print("datatype of num_float:", type(num_float))
+
+new_num = num_int + num_float
+print(new_num)
+print("datatype of new_num is", type(new_num))
+
+my_float = 66.999999
+
+my_num = int(my_float)
+print(my_num)
+
+a = 125.0
+b = 390.8
+
+print(int(a))
+print(int(b))
+
+print(int('12'))
