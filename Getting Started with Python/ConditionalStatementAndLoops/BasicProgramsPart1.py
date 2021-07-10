edfafcdf0a660eaaf8ed57613e1c94de01fd4f23
Anil George <anil.george27@gmail.com>
2021-07-10 09:07:37 +0530

    BasicProgramsPart1

---
+++ Getting Started with Python/ConditionalStatementAndLoops/BasicProgramsPart1.py
@@ -0,0 +1,37 @@
+salary = 100000
+expenses = 35468
+savings = salary - expenses
+print("My total savings in a month is:", savings)
+
+first_num = 12
+second_num = 23
+first_num = first_num + second_num
+second_num = first_num - second_num
+first_num = first_num - second_num
+
+print("\nFirst_num is:", first_num , "\nSecond_num is: ", second_num)
+
+num = 23
+temp1 = str(num)
+temp2 = temp1 + temp1
+temp3 = temp1 + temp1 + temp1
+
+total = num +(int(temp2) + int(temp3))
+print(temp1, '+', temp2, '+', temp3, '=', total)
+
+num_1 = 45
+num_2 = 12
+
+quotient = int(num_1 / num_2)
+print("\nquotient:", quotient)
+
+remainder = num_1 % num_2
+print("Remainder:", remainder)
+
+principle = 1000
+time = 5
+rate = 3
+
+simple_int = (principle * time * rate) / 100
+print("The simple interest is:", simple_int)
+
