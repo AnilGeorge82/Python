a7d57aa44ccc9ccc431d1baa0e8a708a2f9cfa91
Anil George <anil.george27@gmail.com>
2021-07-23 08:53:53 +0530

    ForLoopsIteratingOverElementsInList

---
+++ Getting Started with Python/ConditionalStatementAndLoops/ForLoopsIteratingOverElementsInList.py
@@ -0,0 +1,16 @@
+for letter in ['H', 'e', 'l', 'l', 'o']:
+    print(letter)
+
+for letter in "Hello":
+    print(letter)
+
+my_string = "Hello World"
+
+for char in my_string:
+    print(char)
+
+for digit in "10":
+    print(digit)
+
+for digit in 10:
+    print(digit)
