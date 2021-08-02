29da688d24ecda7cef98a44866a555fb3366d6f4
Anil George <anil.george27@gmail.com>
2021-08-02 09:05:24 +0530

    ForLoopsRangeFunctions

---
+++ Getting Started with Python/ConditionalStatementAndLoops/ForLoopsRangeFunctions.py
@@ -0,0 +1,16 @@
+my_range = range(5)
+print(my_range)
+print(tuple(my_range))
+print(list(range(5,10)))
+print(list(range(-5,5)))
+
+for i in range (5, 10):
+    print(i)
+
+
+for i in range (6):
+    print(i)
+
+
+for i in range (3):
+    print("Hello")
