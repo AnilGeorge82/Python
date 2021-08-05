fa964f13629d0ff753cfbfa8e4852af66bdc5dec
Anil George <anil.george27@gmail.com>
2021-08-05 09:00:39 +0530

    ForLoopsSettingExploringRangeFunction

---
+++ Getting Started with Python/ConditionalStatementAndLoops/ForLoopsSettingExploringRangeFunction.py
@@ -0,0 +1,31 @@
+for i in range (1, 10):
+    if i % 2 == 0:
+        print("The number %s is divisible by 2" % i)
+    else:
+        print("The number %s is not divisible by 2" % i)
+
+num_list = []
+for i in range(1, 21):
+    if(i%3==0 or i%5==0):
+        num_list.append(i)
+print(num_list)
+
+items = []
+for i in range(100, 301):
+    num_str = str(i)
+
+    first_digit = int(num_str[0])
+    second_digit = int(num_str[1])
+    third_digit = int(num_str[2])
+
+    if (first_digit % 2 == 0) and (second_digit % 2 == 0) and (third_digit % 2 == 0):
+        items.append(num_str)
+
+print(", ".join(items))
+
+for num1 in range(1, 10):
+    print("num = ", num1, ":", end = " ")
+
+    for num_2 in range (1, 10):
+        print("{: 2d}".format(num1 * num_2), end = " ")
+    print()
