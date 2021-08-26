8015b4de0add0f5ab0c2a90564fca6cd64385a9b
Anil George <anil.george27@gmail.com>
2021-08-26 09:11:03 +0530

    ForLoopsContinueStatement

---
+++ Getting Started with Python/ConditionalStatementAndLoops/ForLoopsContunueStatement.py
@@ -0,0 +1,27 @@
+for letter in "string" :
+    if letter == "i":
+        continue
+    print(letter)
+print("The end")
+
+my_string = "Students"
+for letter in my_string:
+    if(letter == "e" or letter == "s"):
+        continue
+    else:
+        print(letter)
+
+for i in range(1, 10):
+    if i % 2 == 0:
+        print ("Is %s is an even number? " % i)
+    else:
+        continue
+    print("Yes")
+
+for num in range(1, 10):
+    if num % 2 == 0:
+        print("Found an even number:", num)
+
+        continue
+    print(num)
+
