81caa40e89ac35f28f626ca43d33fce8de6cdfa6
Anil George <anil.george27@gmail.com>
2021-08-27 09:12:11 +0530

    ForLoopsContinueStatement_II

---
+++ Getting Started with Python/ConditionalStatementAndLoops/ForLoopsContunueStatement_II.py
@@ -0,0 +1,37 @@
+for letter in "string" :
+    if letter == "i":
+        continue
+    print(letter)
+print("The end")
+
+my_string = "Students"
+for letter in my_string:
+    if(letter == "e" or letter == "s"):
+        continue
+    else:
+        print(letter)
+
+for i in range(1, 10):
+    if i % 2 == 0:
+        print ("Is %s is an even number? " % i)
+    else:
+        continue
+    print("Yes")
+
+for num in range(1, 10):
+    if num % 2 == 0:
+        print("Found an even number:", num)
+
+        continue
+    print(num)
+
+for i in range(1, 12):
+    print()
+    print("Before the continue statement", i)
+
+    if i % 3 == 0:
+        continue
+    print("After the continue statement", i)
+
+
+num_days = [31, 28, 31,30 ,31]
