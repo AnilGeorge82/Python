0d3aaed8b8ad7b9fcd9031c3b017b23d98945fac
Anil George <anil.george27@gmail.com>
2021-09-07 19:30:34 +0530

    ForLoopsPassStatement

---
+++ Getting Started with Python/ConditionalStatementAndLoops/ForLoopsPassStatement.py
@@ -0,0 +1,37 @@
+for letter in "string":
+    if letter == "r":
+        pass
+
+    print(letter)
+
+for letter in "string":
+    if letter == "r":
+        pass
+        print("This is the pass block")
+    else:
+
+        print("Current letter: ", letter)
+print("Out of the loop")
+
+for i in range(10):
+    if(i % 2 == 0):
+        pass
+    else:
+        print(i)
+
+odd_count = 0
+total_count = 0
+
+for i in range(10):
+    if(i % 2 == 0):
+        pass
+    else:
+        print(i)
+        odd_count += 1
+    total_count += 1
+
+    print("Count of odd numbers: ", odd_count)
+    print("Total count of numbers: ", total_count, "\n")
+
+
+
